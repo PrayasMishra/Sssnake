06446baccc649cdec77af17b7618d93078e38edc
Prayas Mishra <prayas21mishra@gmail.com>
2020-06-24 13:29:52 +0530

    #2 draw.js added

---
+++ draw.js
@@ -0,0 +1,6 @@
+const canvas = document.querySelector(".canvas");
+const ctx = canvas.getContext("2d");
+//console.log(canvas)
+const scale = 10
+const rows = canvas.height/scale;
+const columns = canvas.width/scale;
